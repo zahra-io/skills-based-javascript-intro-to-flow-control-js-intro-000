78f39acb2f473335b2578b77f6f499beb466eff6
Zahra Khan <zarssskhan@gmail.com>
2017-09-11 05:22:40 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -16,7 +16,7 @@ function ageChecker(age) {
   if (age >= 13 && age <= 19) {
     return "You are a teenager!";
   } else if (age <= 12) {
-    return "You are a kid"
+    return "You are a kid";
   }
 };
 
