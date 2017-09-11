790cea957cd339e538b8776787fcc5962dac48e6
Zahra Khan <zarssskhan@gmail.com>
2017-09-11 05:18:22 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -1,20 +1,21 @@
 function basicTeenager(age) {
-  if(age =>  )
-
-};
+  if(age >= 13 && <= 19) {
+    
+  }
+}
 
 function teenager(age) {
 
-};
+}
 
 function ageChecker(age) {
 
-};
+}
 
 function ternaryTeenager(age) {
 
-};
+}
 
 function switchAge(age) {
 
-};
+}
