c3cbeeb9af179b3d822666544775d2873ecbcb00
Zahra Khan <zarssskhan@gmail.com>
2017-09-11 05:37:15 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -31,24 +31,26 @@ function switchAge(age) {
     case: 13;
       return "You are a teenager";
       break;
-    case: 13;
+    case: 14;
       return "You are a teenager";
       break;
-    case: 13;
+    case: 15;
       return "You are a teenager";
       break;
-    case: 13;
+    case: 16;
       return "You are a teenager";
       break;
-    case: 13;
+    case: 17;
       return "You are a teenager";
       break;
-    case: 13;
-        return "You are a teenager";
-        break;
-    case: 13;
-        return "You are a teenager";
-        break;
+    case: 18;
+      return "You are a teenager";
+      break;
+    case: 19;
+      return "You are a teenager";
+      break;
+    default:
+      
 
   }
 }
