8dc5802379f356bfdf458490fc53c23eccadbf90
Zahra Khan <zarssskhan@gmail.com>
2017-09-11 05:31:58 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -23,7 +23,7 @@ function ageChecker(age) {
 };
 
 function ternaryTeenager(age) {
-  return "You are a teenager" ? age >= 13 : age <= 19;
+  age >= 13 && age <= 19 ? return "You are a teenager" : "You are not a teenager"
 };
 
 function switchAge(age) {
